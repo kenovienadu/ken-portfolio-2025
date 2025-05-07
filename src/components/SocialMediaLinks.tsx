d0f330732df4e5093ecab32017af757f4ec63604
Ken Ovienadu <kenovienadu@gmail.com>
2025-05-07 14:16:06 +0100

    feat: add social media links with updated URLs and change link elements to anchor tags

--- src/components/SocialMediaLinks.tsx
+++ src/components/SocialMediaLinks.tsx
@@ -4,22 +4,27 @@ const links = [
   {
     name: 'github',
     iconClass: 'fa-brands fa-github',
+    url: 'https://github.com/kenovienadu'
   },
   {
     name: 'linkedin',
     iconClass: 'fa-brands fa-linkedin',
+    url: 'https://linkedin.com/in/kenovienadu',
   },
   {
     name: 'twitter-x',
     iconClass: 'fa-brands fa-x-twitter',
+    url: 'https://x.com/kenovienadu?s=21'
   },
   {
     name: 'instagram',
     iconClass: 'fa-brands fa-instagram',
+    url: 'https://www.instagram.com/ken_ovienadu?igsh=ZGhjZHZodDBzYXFr&utm_source=qr',
   },
   {
     name: 'medium',
     iconClass: 'fa-brands fa-medium',
+    url: 'https://medium.com/@kenovienadu'
   },
 ]
 
@@ -29,15 +34,15 @@ type Props = {
 
 export default function SocialMediaLinks(props: Props) {
   const wrapperClassName = useMemo(() => {
-    const baseClasses = "flex text-xl md:text-2xl gap-4 md:gap-8 opacity-65";
+    const baseClasses = "flex text-2xl md:text-2xl gap-8 md:gap-10 opacity-65";
     return props.centered ? `${baseClasses} justify-center` : baseClasses;
   }, [props.centered]);
 
   const linkItems = links.map((item, index) => {
     return (
-      <span key={index}>
+      <a key={index} href={item.url} target={'_blank'} className="cursor-pointer">
         <i className={item.iconClass} ></i>
-      </span>
+      </a>
     )
   });
 
