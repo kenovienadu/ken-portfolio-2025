bcb42ddf34dd57c8fd6f9952b3a460d99e5a6990
Ken Ovienadu <kenovienadu@gmail.com>
2025-05-07 15:32:27 +0100

    feat: add profile image and enhance text layout in SideNav component

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import NavTabs from "@/components/NavTabs";
 import SocialMediaLinks from "@/components/SocialMediaLinks";
 import Footer from "@/components/Footer";
@@ -8,13 +10,21 @@ export default function SideNav() {
       {/* Basic Intro */}
       <section>
         <div>
-          <div className="text-5xl font-black mb-2">Ken Ovienadu</div>
+          <div>
+            <Image
+              src="/images/profile.png"
+              alt="Display image for ken ovienadu"
+              width={200}
+              height={300}
+              className="rounded-lg"
+            />
+          </div>
+          <div className="text-5xl font-black my-2 mt-4">Ken Ovienadu</div>
           <div className="font-extrabold mb-3" >Senior FullStack Engineer</div>
-          <div className="opacity-60" >I build accessible, dynamic and performant digital experiences for the web.</div>
-        </div>
-
-        <div className="mt-20">
-          <NavTabs />
+          <div className="opacity-60 mb-2" >I build accessible, dynamic and performant digital experiences for the web.</div>
+          <div className="mt-10">
+            <NavTabs />
+          </div>
         </div>
       </section>
 
