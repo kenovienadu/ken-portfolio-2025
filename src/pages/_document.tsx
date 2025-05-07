86bd9a3bc5e9a74637b7e6e07033573ff8c41cf7
Ken Ovienadu <kenovienadu@gmail.com>
2025-05-07 02:11:29 +0100

    update _document.tsx to format stylesheet link and add page title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
-              integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
-              crossOrigin="anonymous"/>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+          integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+          crossOrigin="anonymous"
+        />
+        <title>Ken Ovienadu | Senior FullStack Engineer</title>
       </Head>
       <body className="antialiased">
       <Main/>
