2e750b352fab812f218e07ec0d97ce260fdf2a24
Ken Ovienadu <kenovienadu@gmail.com>
2025-05-07 10:22:51 +0100

    refactor: adjust layout styles for responsive design in MainLayout component

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,11 +3,11 @@ import SideNav from "@/components/SideNav";
 
 export default function MainLayout(props: PropsWithChildren) {
   return (
-    <main className="mx-30 py-30 flex gap-5 justify-between xl:w-[1200px] xl:mx-auto" >
-      <section className="sticky top-30 h-[80vh] w-2/5" >
+    <main className="mx-5 md:mx-30 py-30 md:flex gap-5 justify-between xl:w-[1200px] xl:mx-auto" >
+      <section className="sticky top-30 h-[80vh] w-2/5 hidden md:inline-block" >
         <SideNav />
       </section>
-      <section className="w-3/5" >
+      <section className="md:w-3/5" >
         {props.children}
       </section>
     </main>
